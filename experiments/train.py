d8f562da0e20cb848483a7c8f0240d3118dce89f
Ahmed Agha <ahmed.agha@argmax.ai>
2024-01-14 13:15:27 +0100

    grab params directly in train.py

--- experiments/train.py
+++ experiments/train.py
@@ -1,7 +1,18 @@
+import clearml
 import train_utils as train_utils
 
 if __name__ == "__main__":
     config = train_utils.make_config(train_utils.define_config())
+    if not config.local:
+        task = clearml.Task.init()
+        task_logger = task.get_logger()
+        task_params = task.get_parameters_as_dict(cast=True)
+        print(task_params)
+        d = task_params["internal"]
+        print(d)
+        config.log_dir = d["log_dir"]
+        config.environment = d["environment"]
+
     from la_mbda.la_mbda import LAMBDA
 
     train_utils.train(config, LAMBDA)
